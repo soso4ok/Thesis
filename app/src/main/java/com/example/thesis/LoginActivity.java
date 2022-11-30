23cefa68d75ad3f40b72531e5ab60c94bc7e87d7
“Volodia” <nxdgbccvbnnnn@gmail.com>
2022-11-30 19:25:33 +0400

    start

---
+++ app/src/main/java/com/example/thesis/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.thesis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button sing_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login_btn);
+        sing_up = findViewById(R.id.singup_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
