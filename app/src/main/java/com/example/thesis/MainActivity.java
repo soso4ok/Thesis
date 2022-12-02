de9e4bbf380cfeebc09070d50f76913980bb1960
“Volodia” <nxdgbccvbnnnn@gmail.com>
2022-12-02 20:27:24 +0400

    Make a bottom menu and main fragment

--- app/src/main/java/com/example/thesis/MainActivity.java
+++ app/src/main/java/com/example/thesis/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.thesis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.sql.Struct;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.botton_menu);
+        bottomNav.setOnNavigationItemSelectedListener(navLisener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navLisener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.home_plane:
+                            selectFragment =  new HomeFragment();
+                            break;
+                        case R.id.qr_code_plane:
+                            selectFragment = new QrCodeFragment();
+                            break;
+                        case R.id.locate_plane:
+                            selectFragment = new LocationFragment();
+                            break;
+                        case R.id.user_plane:
+                            selectFragment = new UserFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectFragment).addToBackStack(null).commit();
+                            return true;
+                }
+
+
+            };
+
 }
